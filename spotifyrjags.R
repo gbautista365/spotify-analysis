4489ec5560e43ff1efb10f1684dd52a90fa7d6c6
germanbautista <gbautista365@gmail.com>
2021-12-14 21:33:32 -0600

    Deleted the old hierarchical.bug file. Revising the RJAGS code right now.

--- spotifyrjags.R
+++ spotifyrjags.R
@@ -1,8 +1,14 @@
 library(rjags)
-#Read the data in. I made the data from the Stream Dataset with adjusted genres into a txt. Theres also a grouped dataset that isn't used 
+
+# Read the data in. 
+# I made the data from the Stream Dataset with adjusted genres into a txt. 
+# Theres also a grouped dataset that isn't used 
 data = read.table("ungrouped_spotify_data.txt")
 
-#Make the streams per day variable. I changed streams to thousands of streams per day for no other reason than it was a random thing I did to try to solve/narrowing down the issues with my code. We can change it back pretty easily
+#Make the streams per day variable. 
+# I changed streams to thousands of streams per day for 
+# no other reason than it was a random thing I did to try to solve/narrowing down the issues with my code. 
+# We can change it back pretty easily
 data$streams_day = data$stream/data$days
 
 #This orders by genre so the newest bugs model makes since
@@ -14,15 +20,19 @@ rownames(data) = NULL
 #This is just here since to show why I put the values I did in the bugs model variable ind
 indicator = c(match(unique(data$top.genre), data$top.genre),length(data[,1])+1)
 
-#The bug model is weird, but essentially I coded it so each genre has its own loop over it's songs. Please check that it makes sense
-#a and b needed priors in order for the model to work I think. I'm not sure I got the priors/hyperpriors assigned correctly
-#I got ride of N in our model since it doesn't really make sense for this model since our data doesn't really have "trials" per se
+#The bug model is weird, but essentially I coded it so each genre has its own loop over it's songs. 
+#Please check that it makes sense
+#a and b needed priors in order for the model to work I think. 
+# I'm not sure I got the priors/hyperpriors assigned correctly
+# I got ride of N in our model since it doesn't really make sense for this model since
+# our data doesn't really have "trials" per se
 
 
 #Assign the thousands of streams per day to Y
 d <- list(Y=data[,4])
 
-#I set random priors for a and b. I think those are the only ones that need it. Feel free to change the numbers when testing
+#I set random priors for a and b. I think those are the only ones that need it.
+# Feel free to change the numbers when testing
 inits <- list(list(a=.0001, b=.0001), 
               list(a=1000, b=1000), 
               list(a=.0001, b=1000))
